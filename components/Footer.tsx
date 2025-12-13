048f9a367ca87459d4bb939187dc0caaa655e9a1
imnadsa <antosh1109@mail.ru>
2025-12-14 03:06:32 +0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
